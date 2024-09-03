88547ff8a3cefc14460cb88f2416773a849ef395
fathanh123 <fathanh.321@gmail.com>
2024-09-04 01:26:30 +0700

    make it mine 4
    masih coba coba fitur popup box di proyek section

--- dist/js/script.js
+++ dist/js/script.js
@@ -52,3 +52,21 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
 } else {
   darkToggle.checked = false;
 }
+
+function openModal(modalId) {
+  document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+  document.getElementById(modalId).style.display = "none";
+}
+
+// Optional: Close the modal when clicking outside of it
+window.onclick = function(event) {
+  const modals = document.getElementsByClassName('modal');
+  for (let i = 0; i < modals.length; i++) {
+    if (event.target == modals[i]) {
+      modals[i].style.display = "none";
+    }
+  }
+}
